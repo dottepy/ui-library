07177b3c45f69992f73e7dbb80fba53153a9158c
dottepy <fadhali.helmizaki@gmail.com>
2026-06-08 20:11:42 +0200

    Update uilibrary.lua

--- uilibrary.lua
+++ uilibrary.lua
@@ -99,7 +99,7 @@ function NeonFlow:CreateWindow(options)
     Create("TextLabel", { Text = title, Font = Enum.Font.GothamBold, TextSize = 13, TextColor3 = Theme.TextPrimary, Size = UDim2.new(0, 0, 1, 0), AutomaticSize = Enum.AutomaticSize.X, BackgroundTransparency = 1, Parent = TitleContainer })
 
     -- Controls Section (Sleek & Neat)
-    local WindowOpts = Create("Frame", { Name = "WindowOpt", Size = UDim2.fromOffset(60, 40), Position = UDim2.new(1, -60, 0, 0), BackgroundTransparency = 1, Parent = TopBar })
+    local WindowOpts = Create("Frame", { Name = "WindowOpt", Size = UDim2.fromOffset(60, 40), Position = UDim2.new(1, -60, 0, -4), BackgroundTransparency = 1, Parent = TopBar })
     Create("UIListLayout", {FillDirection = Enum.FillDirection.Horizontal, HorizontalAlignment = Enum.HorizontalAlignment.Right, VerticalAlignment = Enum.VerticalAlignment.Center, Padding = UDim.new(0, 10), Parent = WindowOpts})
     Create("UIPadding", {PaddingRight = UDim.new(0, 12), Parent = WindowOpts})
 
